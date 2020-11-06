d8d87831c190abbce4935399b8cacbcd03254644
Vyron Korpas <vk@divbyzero.gr>
2020-11-06 13:00:10 +0200

    up to lesson 85

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+
+import './sign-in-and-sign-up.style.scss'
+
+const SingInAndSignUpPage = () => (
+    <div className="sing-in-and-sing-up">
+        <SignIn/>
+    </div>
+)
+
+export default SingInAndSignUpPage
